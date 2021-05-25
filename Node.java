e5aa7761f306d6a7973254edb5ce663969a61f2c
Haidar27-rsyq <72700162+Haidar27-rsyq@users.noreply.github.com>
2021-05-25 23:25:58 +0700

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,44 @@
+package Slinkedlist;
+
+public class Node {
+    protected int data;
+    protected Node link;
+    
+    /* Constuctor */
+    public Node()
+    {
+    link = null;
+    data = 0;
+    }
+    
+    /* Constructor */
+    public Node(int d, Node n)
+    {
+    data = d;
+    link = n;
+    }
+    
+    /* Function to set link to next Node */
+    public void setLink (Node n)
+    {
+    link = n;
+    }
+    
+    /* Function to set data to current Node */
+    public void setData (int d)
+    {
+    data = d;
+    }
+    
+    /* Function to get link from current Node */
+    public Node getLink()
+    {
+    return link;
+    }
+    
+    /* Function to get data from current Node */
+    public int getData()
+    {
+    return data;
+    }
+}
